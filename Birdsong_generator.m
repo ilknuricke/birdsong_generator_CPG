ac66f620bce25e842b6f3d33ddc90c49cd234761
ILKNUR ICKE <ilknur.icke@gmail.com>
2021-03-05 16:45:41 -0500

    Update Birdsong_generator.m

--- Birdsong_generator.m
+++ Birdsong_generator.m
@@ -10,7 +10,7 @@ Fs = 22050;  %sampling
 
 %syllable a
 disp('Generating Syllable a (rho2=-11)');
-[m1 s1]=singSyllable(0.3,-11.0);
+[m1 s1]=singSyllable(-11.0);
 disp('Syllable a generated');
 
 %play the syllable
@@ -18,14 +18,14 @@ sound(s1(:,1),Fs);
 
 %syllable b
 disp('Generating Syllable b (rho2=-11.8)');
-[m2 s2]=singSyllable(0.3,-11.8);
+[m2 s2]=singSyllable(-11.8);
 disp('Syllable c generated');
 %play the syllable
 sound(s2(:,1),Fs);   
 
 %syllable c
 disp('Generating Syllable b (rho2=-7.1)');
-[m3 s3] = singSyllable(0.3,-7.1);
+[m3 s3] = singSyllable(-7.1);
 disp('Syllable c generated');
 %play the syllable
 sound(s3(:,1),Fs);   
